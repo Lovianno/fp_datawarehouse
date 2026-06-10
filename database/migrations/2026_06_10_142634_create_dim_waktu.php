891946b5fd7206d45da27a5975c785be475e9314
UrayMR <umrajsyafiq@gmail.com>
2026-06-10 21:40:10 +0700

    feat: add initial models, controllers, migrations, and seeders for Pelanggan, Produk, and Penjualan

---
+++ database/migrations/2026_06_10_142634_create_dim_waktu.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dim_waktu', function (Blueprint $table) {
+            $table->id('id_waktu');
+            $table->date('tanggal');
+            $table->integer('tahun');
+            $table->integer('bulan');
+            $table->string('bulan_nama');
+            $table->integer('kuartal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dim_waktu');
+    }
+};
---
+++ database/migrations/2026_06_10_142726_create_fact_penjualan.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fact_penjualan', function (Blueprint $table) {
+            $table->id('id_penjualan');
+            $table->foreignId('id_pelanggan')->constrained('dim_pelanggan', 'id_pelanggan')->onDelete('cascade');
+            $table->foreignId('id_produk')->constrained('dim_produk', 'id_produk')->onDelete('cascade');
+            $table->foreignId('id_waktu')->constrained('dim_waktu', 'id_waktu')->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->decimal('harga_satuan', 10, 2);
+            $table->decimal('total_harga', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fact_penjualan');
+    }
+};
