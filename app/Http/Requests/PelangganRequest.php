8849b0f680126b8479830e5ffeca2eb778997ca5
mahardk <zlfkrrvhx@gmail.com>
2026-06-12 16:56:45 +0700

    Revert "feat: add pelanggan management views"
    This reverts commit 6b3f871317d5a973b1bb152b80c4e35d95dafc21.

--- app/Http/Requests/PelangganRequest.php
+++ app/Http/Requests/PelangganRequest.php
@@ -11,7 +11,7 @@ class PelangganRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
@@ -21,24 +21,8 @@ class PelangganRequest extends FormRequest
      */
     public function rules(): array
     {
-        $id = $this->route('pelanggan')?->id_pelanggan;
         return [
-            'kode_pelanggan' => ['required', 'string', 'max:50', 'unique:pelanggan,kode_pelanggan,' . $id . ',id_pelanggan'],
-            'nama_pelanggan' => ['required', 'string', 'max:255'],
-            'jenis_kelamin'  => ['required', 'in:L,P'],
-            'kota'           => ['required', 'string', 'max:100'],
-        ];
-    }
-
-    public function messages(): array
-    {
-        return [
-            'kode_pelanggan.required' => 'Kode pelanggan wajib diisi.',
-            'kode_pelanggan.unique'   => 'Kode pelanggan sudah digunakan.',
-            'nama_pelanggan.required' => 'Nama pelanggan wajib diisi.',
-            'jenis_kelamin.required'  => 'Jenis kelamin wajib dipilih.',
-            'jenis_kelamin.in'        => 'Jenis kelamin tidak valid.',
-            'kota.required'           => 'Kota wajib diisi.',
+            //
         ];
     }
 }
